659ce09c3403b3ff7e0625365b0f23a3b281ac8c
laziza@mail.ru <laziza00>
2022-03-29 07:33:21 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let seeMoadlBtn = document.querySelectorAll('.feature__item-btn')
 
 // ==================feature list write===============
 
-for (let i=0; i<=10; i++) {
+for (let i=0; i<=200; i++) {
     let li = document.createElement('li');
     li.className="feature__item";
     li.innerHTML =`
@@ -43,10 +43,7 @@ for (let i=0; i<=10; i++) {
 
 
 
-
-
-
-// ================== slider list ===============
+// ================== feature slider list ===============
 
 let leftBtn = document.querySelector('#left')
 let rightBtn = document.querySelector('#right')
@@ -161,15 +158,12 @@ heartBtn.forEach(heartBtn => heartBtn.addEventListener('click', (e) =>{
 }
 ))
 
-
-
 let heartArr=[]
-
-
+ 
 
 function AddHeartModal (e) {
 
-  for(let i=0; i<=10; i++) {
+  for(let i=0; i<=200; i++) {
       if(movies[i].imdbId==e) {
         let b = movies[i];    
         heartArr.push(b)
@@ -184,43 +178,39 @@ function AddHeartModal (e) {
     offcanvasItem.className = 'offcanvas__item';
 
     offcanvasItem.innerHTML = `
-              <li class="offcanvas__item">
               <img class="offcanvas__img" src="${item.youtubePoster}" alt="img">
           
               <div style="display:flex;">
               <h3 class="offcanvas__title">"${item.title}"</h3>
               <button class="offcanvas__btn" onclick="removeItemModal('${item.imdbId}')">
               <i class='bx bx-trash'></i></button></div>
-        
-              </li>
             `
             offcanvas__list.appendChild(offcanvasItem)
           })
 }
 
-function removeItemModal (e) {
-
-  let removeArr=[];
-
-  if (confirm("Are you sure you want to delete it? "))
-  {
-    offcanvas__list.innerHTML ="";
-    heartArr.forEach((el)=>{
-          if(movies[i].imdbId!==e) {
-            offcanvas__list.remove(el)
-          }
-      })
-
-  }
-  heartArr = removeArr;
-}
 
 
+// ===================remove item offcanvas=============
 
+// console.log(offcanvas__list);
+// function removeItemModal (e) {
 
+//   let removeArr=[];
 
+//     // offcanvas__list.innerHTML ="";
+//     heartArr.forEach((el,i)=>{
+//       console.log(i);
+//           if(el.imdbId == e) {
+//             heartArr[e].remove
+//             // heartArr.remove(el)
 
+//           }
+//       })
 
+//   // heartArr = removeArr;
+//   AddHeartModal()
+// }
 
 
 
@@ -266,18 +256,11 @@ function openModal(e) {
 
 
 
-
-
-
-
-
-
-
 // ============= video section ===================
 
 let videoList = document.querySelector('#videolist')
 
-for (let k=12; k<=22; k++) {
+for (let k=12; k<=220; k++) {
   let li = document.createElement('li');
   li.className="video__item";
   li.innerHTML =`
@@ -337,288 +320,6 @@ function videoInter() {
 
 
 
-//============= pagination ===========
 
 
-
-  // newMovies= movies.filter((item, index) =>{
-  //   if(index>=32 && index<64){
-  //     return index
-  //   }
-  // })
-  // allList.innerHTML =''
-// for(let i=0; i<newMovies.length; i++) {
-//   let li =document.createElement('li');
-//   li.className= "feature__item";
-//   li.style.margin ="0 20px 20px"
-//   li.innerHTML =
-//   `
-//         <img class="feature__img"src="${movies[i].youtubePoster}" alt="img">
-//         <div style="display: flex;">
-//           <p class="feature__country">USA</p>
-//           <p class="feature__year feature__country">${movies[i].year}</p>
-//         </div>
-
-//         <h3 class="feature__item-title" id="title">${movies[i].title}</h3>
-//         <div style="display: flex; justify-content: space-between; align-items: center; margin: 0 0 12px 0;">
-//           <div style="display: flex; align-items: center;">
-//             <img class="feature__img-img" src="images/imd-img.png" alt="img">
-//             <p class="feature__number">86/100</p>
-//           </div>
-
-//           <div style="display: flex; align-items: center;">
-//           <i class='bx bxs-star'></i>
-//           <p class="feature__percent">97%</p>
-//         </div>
-//         </div>
-//         <div style="display: flex;">
-//           <p class="feature__category" id="feature__category">${movies[i].categories},</p>
-//         </div>
-//         <button class="feature__item-btn feature__btn" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="openModal('${movies[i].imdbId}')">See more <i class='bx bxs-chevron-right'></i></button>
-//         <button class="feature__heart-btn" id="heart__btn" onclick="AddHeartModal('${movies[i].imdbId}')">
-//         <i class='bx bxs-heart' ></i>
-//       </button>
-//     `
-//     allList.appendChild(li)
-//     console.log(li);
-// }
-
-
-
-
-// const list__items = [
-
-//   "item 1",
-//   "item 2",
-//   "item 3",
-//   "item 4",
-//   "item 5",
-//   "item 6",
-//   "item 7",
-//   "item 8",
-//   "item 9",
-//   "item 10",
-//   "item 11",
-//   "item 12",
-//   "item 13",
-//   "item 14",
-//   "item 15",
-//   "item 16",
-//   "item 17",
-//   "item 18",
-//   "item 19",
-//   "item 20",
-//   "item 21",
-//   "item 22",
-// ];
-
-
-// const list_element = document.querySelector('#alllist')
-// const pagination_element= document.querySelector('#paglist')
-
-
-// let current_page =1;
-// let rows =5;
-
-// function displayList (items, wrapper, rows_per_page, page){
-//   wrapper.innerHTML ="";
-//   page--;
-
-//   let start =rows_per_page *page;
-//   let end = start +rows_per_page
-//   let paginatedItems = items.slice(start, end)
-
-  
-//   for(let i=0; i<paginatedItems.length; i++){
-//     let item = paginatedItems[i];
-//     let item_element = document.createElement('li');
-//     item_element.classList.add('item');
-//     item_element.innerText = item;
-
-//     wrapper.appendChild(item_element)
-//   }
-
-// }
-
-
-// function setupPagination(items, wrapper, rows_per_page) {
-//   wrapper.innerHTML ="";
-
-
-//   let page_count = Math.ceil(items.length/rows_per_page)
-
-//   for(let i=1; i<page_count+1; i++) {
-//     let btn = paginationButton(i, items)
-//     wrapper.appendChild(btn)
-
-//   }
-// }
-
-// function paginationButton(page, items) {
-//   let button = document.createElement('button');
-//   button.innerText = page;
-
-
-//   if(current_page ==page) button.classList.add('active');
-//     button.addEventListener('click', function(){
-//       current_page=page;
-//       displayList(items, list_element, rows, current_page);
-
-//       let current_btn =document.querySelector('.pag__list button.active');
-//       current_btn.classList.remove('active')
-    
-//       button.classList.add('active')
-//     })
-//     return button
-// }
-
-
-
-// displayList(list__items, list_element,rows, current_page )
-// setupPagination(list__items, pagination_element, rows)
-
-
-
-
-
-
-
-
-
-
-let allList = document.querySelector('#alllist')
-let pagList = document.querySelector('#paglist')
-let allPages = 15
-
-function elem(allPages, page) {
-
-  let li ='';
-
-  let beforePages = page-1;
-  let afterPages = page +1 
-  let liActive;
-
-
-
-if(page >1) {
-    li+= `
-    <li class=" pag__icon  pag__btn" style="border-radius: 50%;" onclick ="elem(allPages, ${page-1})" data-name="prevbtn">
-    <i class='bx bx-chevron-left' ></i>
-  </li>`
-}
-
-for (let pageLength = beforePages; pageLength<=afterPages; pageLength++){
-
-    if(pageLength>allPages) {
-      continue;
-    }
-    if(pageLength==0) {
-      pageLength=pageLength+1
-    }
-    if(page==pageLength) {
-      liActive = 'active'
-    }else {
-      liActive = ''
-    }
-
-  li+= `<li class="pag__numb  ${liActive}" onclick ="elem(allPages, ${pageLength})" data-name="two">
-  <span> ${pageLength}</span>
- </li>
-  `
-
-}
-
-    if(page < allPages) {
-      li += `  <li class="pag__icon  pag__btn" onclick ="elem(allPages, ${page+1})"style="border-radius: 50%;" id="nextbtn" data-name="nextbtn">
-      <i class='bx bx-chevron-right'></i>
-    </li>`
-    }
-  pagList.innerHTML = li
-}
-elem(allPages,2)
-
-
-
-newMovies =[]
-nextBtn =document.querySelector('#nextbtn');
-
-nextBtn.addEventListener('click', () => {
-  WorkNextBtn();
-})
-
-
-function WorkNextBtn () {
-  
-  newMovies= movies.filter((item, index) =>{
-    if(index>=32 && index<64){
-      return index
-    }
-  })
-  allList.innerHTML =''
-for(let i=0; i<newMovies.length; i++) {
-  let li =document.createElement('li');
-  li.className= "feature__item";
-  li.style.margin ="0 20px 20px"
-  li.innerHTML =
-  `
-        <img class="feature__img"src="${movies[i].youtubePoster}" alt="img">
-        <div style="display: flex;">
-          <p class="feature__country">USA</p>
-          <p class="feature__year feature__country">${movies[i].year}</p>
-        </div>
-
-        <h3 class="feature__item-title" id="title">${movies[i].title}</h3>
-        <div style="display: flex; justify-content: space-between; align-items: center; margin: 0 0 12px 0;">
-          <div style="display: flex; align-items: center;">
-            <img class="feature__img-img" src="images/imd-img.png" alt="img">
-            <p class="feature__number">86/100</p>
-          </div>
-
-          <div style="display: flex; align-items: center;">
-          <i class='bx bxs-star'></i>
-          <p class="feature__percent">97%</p>
-        </div>
-        </div>
-        <div style="display: flex;">
-          <p class="feature__category" id="feature__category">${movies[i].categories},</p>
-        </div>
-        <button class="feature__item-btn feature__btn" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="openModal('${movies[i].imdbId}')">See more <i class='bx bxs-chevron-right'></i></button>
-        <button class="feature__heart-btn" id="heart__btn" onclick="AddHeartModal('${movies[i].imdbId}')">
-        <i class='bx bxs-heart' ></i>
-      </button>
-    `
-    allList.appendChild(li)
-    console.log(li);
-}
-
-}
-
-
-
-
-// =====================input search==================
-
-let inputSearch = document.querySelector('#inputsearch')
-
-inputSearch.addEventListener('keyup', filterItems);
-
-function filterItems(e){
-
-  let text = e.target.value.toLowerCase();
-
-  let items =moviesList.getElementsByTagName('li');
-
-  if(text!='') {
-
-    for (let i=0; i<Array.from(items); i++){
-
-        let itemName = items[i].firstElementChild.textContent;
-        if(itemName.toLocaleLowerCase().indexOf(text) !=-1) {
-          items[i].style.color= 'yellow'
-        }else {
-          items[i].style.display= 'none'
-        }
-    }
-  }
-}
 
---
+++ js/movies.js
@@ -0,0 +1,282 @@
+// =====================future search==================
+
+
+let newCategory =[]
+let featureNewList = document.querySelector('#featurenewlist')
+let formCategory = document.querySelector('#formcategory')
+let formSearchForm = document.querySelector('#formsearch')
+let searchMovie = document.querySelector('#searchmovie');
+let searchStart = document.querySelector('#searchstart');
+let searchEnd = document.querySelector('#searchend');
+
+
+
+
+formSearchForm.addEventListener('submit', filterSearch);
+
+
+
+function filterSearch (e) {
+  e.preventDefault();
+
+  featureNewList.innerHTML =""
+
+    let fSearchInput= searchMovie.value
+    let fCategory  =formCategory.value
+    let fSearchStart = searchStart.value
+    let fSearchEnd = searchEnd.value
+ 
+
+  for(let i=0; i<movies.length; i++) {
+
+    if(movies[i].categories.includes(fCategory) &&
+    movies[i].title.toLowerCase().includes(fSearchInput.toLowerCase()) 
+    && fSearchStart <=movies[i].year && fSearchEnd>=movies[i].year) {
+      console.log("nimadir");
+    
+      let li =document.createElement('li');
+      li.className ="feature__item"
+      li.innerHTML = `
+      
+    <img class="feature__img"src="${movies[i].youtubePoster}" alt="img">
+    <div style="display: flex;">
+      <p class="feature__country">USA</p>
+      <p class="feature__year feature__country">${movies[i].year}</p>
+    </div>
+
+    <h3 class="feature__item-title" id="title">${movies[i].title}</h3>
+    <div style="display: flex; justify-content: space-between; align-items: center; margin: 0 0 12px 0;">
+      <div style="display: flex; align-items: center;">
+        <img class="feature__img-img" src="images/imd-img.png" alt="img">
+        <p class="feature__number">86/100</p>
+      </div>
+
+      <div style="display: flex; align-items: center;">
+      <i class='bx bxs-star'></i>
+      <p class="feature__percent">97%</p>
+    </div>
+    </div>
+    <div style="display: flex;">
+      <p class="feature__category" id="feature__category">${movies[i].categories},</p>
+    </div>
+    <button class="feature__item-btn feature__btn" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="openModal('${movies[i].imdbId}')">See more <i class='bx bxs-chevron-right'></i></button>
+    <button class="feature__heart-btn" id="heart__btn" onclick="AddHeartModal('${movies[i].imdbId}')">
+    <i class='bx bxs-heart' ></i>
+  </button>`
+  featureNewList.appendChild(li)
+    }
+      searchMovie.value =""
+      formCategory.value ="All"
+      searchStart.value=""
+      searchEnd.value=""
+  }
+
+}
+
+
+
+for(let l=0; l<movies.length; l++) {
+  let inputArr = movies[l].categories;
+
+  for(let k=0; k<inputArr.length; k++) {
+    if(!newCategory.includes(inputArr[k])){
+      newCategory.push(inputArr[k])
+    }
+  }
+}
+for(let f=0; f<newCategory.length; f++) {
+  let newOption = document.createElement('option');
+  newOption.innerHTML = newCategory[f];
+  
+  formCategory.appendChild(newOption)
+}
+
+
+
+
+
+
+
+
+
+
+//============= pagination ===========
+
+
+
+let allList = document.querySelector('#alllist')
+let pagList = document.querySelector('#paglist')
+let nextBtnPag = document.querySelector('#nextbtn')
+let allPages = 127
+
+function elem(allPages, page) {
+
+  let li ='';
+
+  let beforePages = page-1;
+  let afterPages = page +1 
+  let liActive;
+
+
+
+if(page >1) {
+    li+= `
+    <li class=" pag__icon  pag__btn" style="border-radius: 50%;" onclick ="elem(allPages, ${page-1})" data-name="prevbtn">
+    <i class='bx bx-chevron-left' ></i>
+  </li>`
+}
+
+for (let pageLength = beforePages; pageLength<=afterPages; pageLength++){
+
+    if(pageLength>allPages) {
+      continue;
+    }
+    if(pageLength==0) {
+      pageLength=pageLength+1
+    }
+    if(page==pageLength) {
+      liActive = 'active'
+    }else {
+      liActive = ''
+    }
+
+  li+= `<li class="pag__numb  ${liActive}" onclick ="elem(allPages, ${pageLength})" data-name="two">
+  <span> ${pageLength}</span>
+ </li>
+  `
+
+}
+
+    if(page < allPages) {
+      li += `  <li class="pag__icon  pag__btn" onclick ="elem(allPages, ${page+1})"style="border-radius: 50%;" id="nextbtn" data-name="nextbtn">
+      <i class='bx bx-chevron-right'></i>
+    </li>`
+    }
+  pagList.innerHTML = li
+}
+elem(allPages,1)
+
+
+let pabBtn = document.querySelector('.pag__numb');
+
+pagList.addEventListener('click', (e)=> {
+
+    let a = e.target.textContent;
+    paginationFor(a)
+})
+
+nextBtnPag.addEventListener('click', (e)=>{
+    page = page+1;
+    paginationFor(page)   
+})
+
+let page =1;
+
+function render(movies) {
+    allList.innerHTML ='';
+
+    for(let i=0; i<32; i++) {
+        let li=document.createElement('li');
+        li.className ="feature__item"
+        li.innerHTML = `
+        
+      <img class="feature__img"src="${movies[i].youtubePoster}" alt="img">
+      <div style="display: flex;">
+        <p class="feature__country">USA</p>
+        <p class="feature__year feature__country">${movies[i].year}</p>
+      </div>
+  
+      <h3 class="feature__item-title" id="title">${movies[i].title}</h3>
+      <div style="display: flex; justify-content: space-between; align-items: center; margin: 0 0 12px 0;">
+        <div style="display: flex; align-items: center;">
+          <img class="feature__img-img" src="images/imd-img.png" alt="img">
+          <p class="feature__number">86/100</p>
+        </div>
+  
+        <div style="display: flex; align-items: center;">
+        <i class='bx bxs-star'></i>
+        <p class="feature__percent">97%</p>
+      </div>
+      </div>
+      <div style="display: flex;">
+        <p class="feature__category" id="feature__category">${movies[i].categories},</p>
+      </div>
+      <button class="feature__item-btn feature__btn" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="openModal('${movies[i].imdbId}')">See more <i class='bx bxs-chevron-right'></i></button>
+      <button class="feature__heart-btn" id="heart__btn" onclick="AddHeartModal('${movies[i].imdbId}')">
+      <i class='bx bxs-heart' ></i>
+    </button>`
+    allList.appendChild(li)
+    }
+}
+render(movies);
+
+function paginationFor(page) {
+    console.log(page);
+
+    let pagArr =[]
+
+    for(let i=((page-1)*32); i<=(page*32); i++) {
+        pagArr.push(movies[i])
+    }
+    render(pagArr)
+}
+
+
+// ==============hero search================
+ let heroSearchForm = document.querySelector('#heroSearchfrom');
+ let heroSearchInput = document.querySelector('#herosearchinput')
+ let heroListSearch = document.querySelector('#herolistSearch')
+
+ heroSearchForm.addEventListener('submit', WorkHeroInput)
+
+ 
+ function WorkHeroInput (e) {
+    e.preventDefault();
+  
+ 
+     heroListSearch.innerHTML =""
+ 
+     let finputSearch= heroSearchInput.value
+  
+ 
+   for(let i=0; i<movies.length; i++) {
+ 
+     if(movies[i].title.toLowerCase().includes(finputSearch.toLowerCase())) {
+     
+       let li =document.createElement('li');
+       li.className ="feature__item"
+       li.innerHTML = `
+       
+     <img class="feature__img"src="${movies[i].youtubePoster}" alt="img">
+     <div style="display: flex;">
+       <p class="feature__country">USA</p>
+       <p class="feature__year feature__country">${movies[i].year}</p>
+     </div>
+ 
+     <h3 class="feature__item-title" id="title">${movies[i].title}</h3>
+     <div style="display: flex; justify-content: space-between; align-items: center; margin: 0 0 12px 0;">
+       <div style="display: flex; align-items: center;">
+         <img class="feature__img-img" src="images/imd-img.png" alt="img">
+         <p class="feature__number">86/100</p>
+       </div>
+ 
+       <div style="display: flex; align-items: center;">
+       <i class='bx bxs-star'></i>
+       <p class="feature__percent">97%</p>
+     </div>
+     </div>
+     <div style="display: flex;">
+       <p class="feature__category" id="feature__category">${movies[i].categories},</p>
+     </div>
+     <button class="feature__item-btn feature__btn" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="openModal('${movies[i].imdbId}')">See more <i class='bx bxs-chevron-right'></i></button>
+     <button class="feature__heart-btn" id="heart__btn" onclick="AddHeartModal('${movies[i].imdbId}')">
+     <i class='bx bxs-heart' ></i>
+   </button>`
+   heroListSearch.appendChild(li)
+     }
+      finputSearch.value =""
+   }
+ 
+ }
+ 
+ 
